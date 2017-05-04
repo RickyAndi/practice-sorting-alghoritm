c472e7dd4f818bea6c0ae8784eb7b14d5ef7b9c6
RickyAndi <ricky3andika@gmail.com>
2017-05-04 13:45:51 +0700

    Add insertion sort

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { quickSort } = require('./src/quick-sort');
 const { mergeSort } = require('./src/merge-sort');
 const { bubbleSort } = require('./src/bubble-sort');
+const { insertionSort } = require('./src/insertion-sort');
 
 module.exports = {
     quickSort,
     mergeSort,
-    bubbleSort
+    bubbleSort,
+    insertionSort
 };
